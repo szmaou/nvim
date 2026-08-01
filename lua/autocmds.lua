5537e479f7be2687c79f7053cc2d24892014da6c
szmaou <szmaou@gmail.com>
2026-08-01 14:59:49 +0700

    remove matugen theme and its SIGUSR1 reload config

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,9 +1,1 @@
 require "nvchad.autocmds"
-
--- Matugen theme reload on SIGUSR1
-vim.api.nvim_create_autocmd("Signal", {
-	pattern = "SIGUSR1",
-	callback = function()
-		require("base46").load_all_highlights()
-	end,
-})
